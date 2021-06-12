309c3d0c1f6b58d3a10d92b0bc9e6025ff6f5340
Kartikey Dubey <dkartikey1@gmail.com>
2021-06-13 00:55:50 +0530

    Made Captcha functionable

--- js/video.js
+++ js/video.js
@@ -1,3 +1,29 @@
+
+// Captcha JS
+var listForCaptcha = "abcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*_:?";
+var temporaryCaptcha = '';
+function generateCaptcha(CaptchaLength) {
+    temporaryCaptcha = '';
+    for (let index = 0; index < CaptchaLength; index++) {
+        temporaryCaptcha += listForCaptcha.charAt(Math.floor(Math.random() * listForCaptcha.length));
+        // console.log(temporaryCaptcha += listForCaptcha.charAt(Math.floor(Math.random()*listForCaptcha.length)));
+        
+    }
+    return temporaryCaptcha;
+}
+
+function printCaptcha(val) {
+    var captcOutput = document.querySelector("#out");
+    console.log(captcOutput);
+    captcOutput.value = generateCaptcha(val);
+    captcOutput.style.fontWeight = "bold";
+  
+}
+
+
+
+
+
 function video_switch(){
     document.querySelector('.video_switch').addEventListener('click',function(){
         let btn = document.querySelector('.video_switch-btn');
